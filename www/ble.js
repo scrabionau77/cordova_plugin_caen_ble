3700539eaafcccb41e2ee24497da799875f7fa57
Daniele Pellerucci <dpellerucci@gmail.com>
2023-09-28 12:29:01 +0200

    migliorie

--- www/ble.js
+++ www/ble.js
@@ -2,31 +2,31 @@ var exec = require('cordova/exec')
 
 var caenBle = {
     discoverDevices: function(successCallback, errorCallback) {
-        cordova.exec(successCallback, errorCallback, "MyBluetoothPlugin", "discoverDevices", []);
+        cordova.exec(successCallback, errorCallback, "MaeCaenBLE", "discoverDevices", []);
     },
 
     connectToDevice: function(address, successCallback, errorCallback) {
-        cordova.exec(successCallback, errorCallback, "MyBluetoothPlugin", "connectToDevice", [address]);
+        cordova.exec(successCallback, errorCallback, "MaeCaenBLE", "connectToDevice", [address]);
     },
 
     stopDiscovery: function(successCallback, errorCallback) {
-        cordova.exec(successCallback, errorCallback, "MyBluetoothPlugin", "stopDiscovery", []);
+        cordova.exec(successCallback, errorCallback, "MaeCaenBLE", "stopDiscovery", []);
     },
 
     disconnectDevice: function(successCallback, errorCallback) {
-        cordova.exec(successCallback, errorCallback, "MyBluetoothPlugin", "disconnectDevice", []);
+        cordova.exec(successCallback, errorCallback, "MaeCaenBLE", "disconnectDevice", []);
     },
 
     requestPermissions: function(successCallback, errorCallback) {
-        cordova.exec(successCallback, errorCallback, "MyBluetoothPlugin", "requestPermissions", []);
+        cordova.exec(successCallback, errorCallback, "MaeCaenBLE", "requestPermissions", []);
     },
 
     startTagCheck: function(successCallback, errorCallback) {
-        cordova.exec(successCallback, errorCallback, "MyBluetoothPlugin", "startTagCheck", []);
+        cordova.exec(successCallback, errorCallback, "MaeCaenBLE", "startTagCheck", []);
     },
 
     stopTagCheck: function(successCallback, errorCallback) {
-        cordova.exec(successCallback, errorCallback, "MyBluetoothPlugin", "stopTagCheck", []);
+        cordova.exec(successCallback, errorCallback, "MaeCaenBLE", "stopTagCheck", []);
     }
 };
 
