84e694feeb6910881e0f95ce2ef3d8b795b96860
Daniele Pellerucci <dpellerucci@gmail.com>
2023-09-28 12:36:14 +0200

    --

--- www/ble.js
+++ www/ble.js
@@ -2,31 +2,31 @@ var exec = require('cordova/exec')
 
 var caenBle = {
     discoverDevices: function(successCallback, errorCallback) {
-        cordova.exec(successCallback, errorCallback, "MaeCaenBLE", "discoverDevices", []);
+        cordova.exec(successCallback, errorCallback, "caenBle", "discoverDevices", []);
     },
 
     connectToDevice: function(address, successCallback, errorCallback) {
-        cordova.exec(successCallback, errorCallback, "MaeCaenBLE", "connectToDevice", [address]);
+        cordova.exec(successCallback, errorCallback, "caenBle", "connectToDevice", [address]);
     },
 
     stopDiscovery: function(successCallback, errorCallback) {
-        cordova.exec(successCallback, errorCallback, "MaeCaenBLE", "stopDiscovery", []);
+        cordova.exec(successCallback, errorCallback, "caenBle", "stopDiscovery", []);
     },
 
     disconnectDevice: function(successCallback, errorCallback) {
-        cordova.exec(successCallback, errorCallback, "MaeCaenBLE", "disconnectDevice", []);
+        cordova.exec(successCallback, errorCallback, "caenBle", "disconnectDevice", []);
     },
 
     requestPermissions: function(successCallback, errorCallback) {
-        cordova.exec(successCallback, errorCallback, "MaeCaenBLE", "requestPermissions", []);
+        cordova.exec(successCallback, errorCallback, "caenBle", "requestPermissions", []);
     },
 
     startTagCheck: function(successCallback, errorCallback) {
-        cordova.exec(successCallback, errorCallback, "MaeCaenBLE", "startTagCheck", []);
+        cordova.exec(successCallback, errorCallback, "caenBle", "startTagCheck", []);
     },
 
     stopTagCheck: function(successCallback, errorCallback) {
-        cordova.exec(successCallback, errorCallback, "MaeCaenBLE", "stopTagCheck", []);
+        cordova.exec(successCallback, errorCallback, "caenBle", "stopTagCheck", []);
     }
 };
 
